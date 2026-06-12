0a9bd30981bc7a34b5643ecc75721af22da03d09
yoshiyoshiyoshi3-glitch <yoshiyoshiyoshi3@gmail.com>
2026-06-12 23:46:05 +0900

    sw: cache 20260612-2345

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shipment-20260612-2011';
+const CACHE = 'shipment-20260612-2345';
 const ASSETS = ['./index.html','./manifest.json','./icon-192.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
